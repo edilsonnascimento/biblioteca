538743058fa9805a21e4f8cf41fd0af87c5e99e4
Edilson do Nascimento <edilsonjoper@gmail.com>
2021-03-09 09:12:49 -0300

    Criação do Projeto

---
+++ src/test/java/br/com/utfpr/jpa/biblioteca/BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.utfpr.jpa.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
